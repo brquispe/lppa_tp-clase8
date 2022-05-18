e0c783afd5ba2b30b272ec7efe064b6cfc3ca635
Braian Quispe <BraianRafael.Quispe@alumnos.uai.edu.ar>
2022-05-18 01:05:04 -0300

    First exercise

---
+++ README.md
@@ -0,0 +1,8 @@
+# FACULTAD DE TECNOLOGIA INFORMATICA
+# INGENIERIA EN SISTEMAS INFORMATICOS
+
+## Lenguajes de programación para la administración
+### Actividad asincrónica, clase 8
+### Alumno: Braian Quispe
+
+La verdad las dificultades que tuve fueron al usar los métodos de strings que hacía mucho que no los utilizaba.
---
+++ exercises/01_variables_and_operators.js
@@ -0,0 +1,9 @@
+const boringString = 'Lorem ipsum dolor sit';
+const boringString2 = 'Lorem ipsum dolor sit amet consectetur';
+const concatenatedBoringStrings = boringString + boringString2;
+const stringsLength = concatenatedBoringStrings.length
+
+console.log('First string: ', boringString);
+console.log('Second string: ', boringString2);
+console.log('Concatenated strings: ', concatenatedBoringStrings);
+console.log('Length of both strings concatenated: ', stringsLength);
